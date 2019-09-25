c845b2c3429a5e55c2a2d5f58c094d883b283836
excavator <str_token@mail.ru>
2019-09-25 16:32:28 +0300

    add jquery and bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,16 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// jQuery
+require("jquery")
+// Bootstrap
+require('bootstrap/dist/js/bootstrap')
+import 'bootstrap/dist/css/bootstrap.min.css';
+//test jquery, pipet.js
+$(function(){
+  $('.jqr').html('Jquery run!');
+  $('[data-toggle="tooltip"]').tooltip();
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +25,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
